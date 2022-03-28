0270953cfeba7bc4d870a3ba1570d7eccb5fe861
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2022-03-28 18:14:23 +0300

    adding to branch

---
+++ IOTSERVER/trialfile.cpp
@@ -0,0 +1,10 @@
+#Includeviewstock:
+cout<<"\n********EXISTING STOCKS*******"<<endl;
+//cout the fle
+
+stockfile>>type;
+stockfile>>quantity;
+stockfile>>price;
+cout<<type<<"  ";
+cout<<quantity<<"  ";
+cout<<price<<
